d65ee8e20e02ec852d14de278b1de3803a0d7232
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-16 22:07:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,12 @@
-# Write your methods here
+def loop_message_five_times(string)
+
+end
+def loop_message_n_times(string, integers)
+
+end
+def output_array(array)
+
+end
+def return_string_array(array)
+
+end # Write your methods here
