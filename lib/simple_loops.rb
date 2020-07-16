0f417c0fbfc6c723025a89f2efda51467a84f07e
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-16 22:51:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,14 +2,14 @@ def loop_message_five_times(message)
  count = 0
 while count < 5 do
   puts message
-  counter += 1
+  count += 1
   end
 end
 def loop_message_n_times(message, limit)
-  counter = 0
-while counter < limit do
+  count = 0
+while count < limit do
   puts message
-  counter += 1
+  count += 1
  end
 end
 def output_array(array)
